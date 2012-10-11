988787bc49139f7626885bfcc676ee29b4b8931c
Dylan Romeo Wahlstrom <dylanwahlstrom@Dylans-MacBook-Pro-2.local>
2012-10-11 00:25:04 -0400

    no message

--- main.js
+++ main.js
@@ -3,74 +3,59 @@
 //wait until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
 
-	function $(x) {
-		var theElement = document.getElementById(x);
-		return theElement;
-	}
-	
-	function getTats(){
-		var formTag = document.getElementsByTagName("form"),
-			selectLi = $('select'),
-			makeSelect = document.createElement('select');
-			makeSelect.setAttribute("id", "groups);
-			var makeOption = document.createElement('option');
-			makeOption.setAttribute("value");
-			makeSelect.appendChild(makeOption);
-		}
-		selectLi.appendChild(makeSelect);
-	}
-	
-	function getSelectedRadio(){
-		var radios = document.forms[0].sex;
-		for(var i=0; i<radios.length; i++){
-			if(radios[i].checked){
-				sexValue = radios[i].value;
-			}
-		}
-	}
-	
-	function storeData(){
-		var id 			= Math.floor(math.random()*1000000001);
-		//gather up all form values and store in an object
-		//Object properties contain array with the form label and input values
-		getSelectedRadios();
-		var item		= {};
-			item.fname	= ["First Name:", $('fname').value];
-			item.lname 	= ["Last Name:", $('lname').value];
-			item.email	= ["Email:", $('email').value];
-			item.sex	= ["Sex:", sexValue];
-			item.url= ["Website:", $('url').value];
-			item.date	= ["Date:", $('date').value];
-			item.pain	= ["Pain Tolerance:", $('pain').value];
-			item.comment= ["Comments:", $('comments').value];
-		//save data into Local Storage
-		localStorage.setItem(id, JSON.stringify(item));
-		alert("Tattoo Appointment Made!");		
-	}
-	var sexValue;
-	getTats();
-	/*function getData(){
-		var makeDiv = document.createElement('div');
-		makeDiv/setAttribute("id", "items");
-		var makeList = document.createElement('ul');
-		makeDiv.appendChild(makeList);
-		for(var i=0, len=localStorage.length; i<len;i++){
-			var makeli = document.createElement('li');
-			makeList.appendChild(makeli);
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
-			var obj = JSON.parse(value);
-	*/
-	
-	
-	//Set link and submit click events
-	/*var displayLink = $('displayLink');
-	displayLink.addEventListener("click", getData);
-	var clearLink = $('clear');
-	clearLink.addEventListener("click", clearLocal);*/
-	var save = $('submit');
-	save.addEventListener("click", storeData);
-	
+    function $(x) {
+        var theElement = document.getElementById(x);
+        return theElement;
+    }
+    
+    function getTats(){
+        var formTag = document.getElementsByTagName("form"),
+            selectLi = $('select'),
+            makeSelect = document.createElement('select');
+            makeSelect.setAttribute("id", "groups");
+	for(var i=0, j=styleGroups.length; i<j; i++){
+            var makeOption = document.createElement('option');
+	    var optText = styleGroups[i];
+            makeOption.setAttribute("value");
+	    makeOption.innerHTML = optText;
+            makeSelect.appendChild(makeOption);
+        }
+        selectLi.appendChild(makeSelect);
+    }
+    
+    
+    function getSelectedRadio(){
+        var radios = document.forms[0].sex;
+        for(var i=0; i<radios.length; i++){
+            if(radios[i].checked){
+                sexValue = radios[i].value;
+            }
+        }
+    }
+    
+    function storeData(){
+        var id             = Math.floor(math.random()*1000000001);
+        getSelectedRadios();
+        var item        = {};
+            item.fname    = ["First Name:", $('fname').value];
+            item.lname     = ["Last Name:", $('lname').value];
+            item.email    = ["Email:", $('email').value];
+            item.sex    = ["Sex:", sexValue];
+            item.url= ["Website:", $('url').value];
+            item.date    = ["Date:", $('date').value];
+            item.pain    = ["Pain Tolerance:", $('pain').value];
+            item.comment= ["Comments:", $('comments').value];
+       
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Tattoo Appointment Made!");        
+    }
+    var styleGroups = ["--Choose A Style--", "Color", "Black & White", "Outline"];   
+	sexValue;
+    getTats ();
+  
+    var save = $('submit');
+    save.addEventListener("click", storeData);
+    });
 
 
 
@@ -79,4 +64,3 @@ window.addEventListener("DOMContentLoaded", function(){
 
 
 
-});
