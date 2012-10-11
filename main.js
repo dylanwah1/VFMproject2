c6d48fee90ce38c53e76739707265c7b211e0bdd
Dylan Romeo Wahlstrom <dylanwahlstrom@Dylans-MacBook-Pro-2.local>
2012-10-11 01:14:00 -0400

    no message

--- main.js
+++ main.js
@@ -33,10 +33,33 @@ window.addEventListener("DOMContentLoaded", function(){
         }
     }
     
+    function toggleControls(n){
+		switch(n){
+			case "on":
+				$('itemForm').style.display = "none";
+				$('clearData').style.display = "inline";
+				$('displayData').style.display = "none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('itemForm').style.display = "block";
+				$('clearData').style.display = "inline";
+				$('displayData').style.display = "inline";
+				$('addNew').style.display = "none";
+				$('items').style.display = "none";
+				break;
+			default:
+				return false;		
+
+		}
+	}
+
+    
     function storeData(){
         var id             = Math.floor(math.random()*1000000001);
-        getSelectedRadios();
+        getSelectedRadio();
         var item        = {};
+	    item.style  = ["Style:", $('groups').value];
             item.fname  = ["First Name:", $('fname').value];
             item.lname  = ["Last Name:", $('lname').value];
             item.email  = ["Email:", $('email').value];
@@ -49,25 +72,57 @@ window.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem(id, JSON.stringify(item));
         alert("Tattoo Appointment Made!");        
     };
-    getData(){
+   function getData(){
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There are no items saved!");
+		}
 		var makeDiv = document.createElement('div');
-		makeDiv/setAttribute("id", "items");
+		makeDiv.setAttribute("id", "items");
 		var makeList = document.createElement('ul');
 		makeDiv.appendChild(makeList);
-		for(var i=0, len=localStorage.length; i<len;i++){
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
+		for(var i=0, len=localStorage.length; i<len; i++){
 			var makeli = document.createElement('li');
 			makeList.appendChild(makeli);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
+//Convert string from local storage value back to object using JSON.parse
 			var obj = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeli.appendChild(makeSubList);
+			for(var n in obj){
+				var makeSubli = document.createElement('li');
+				makeSubList.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+
+			}
+
+		}
+	}
+
+	function deleteData(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear!");
+
+		}else{
+			localStorage.clear();
+			alert("All Items Have Been Deleted!");
+			window.location.reload();
+			return false;
+		}
+	}
     var styleGroups = ["--Choose A Style--", "Color", "Black & White", "Outline"];   
 	sexValue;
     getTats ();
-    
-    
-  
-    var save = $('submit');
-    save.addEventListener("click", storeData);
+        var displayData = $('displayData');
+	displayData.addEventListener("click", getData);
+	var clearData = $('clearData');
+	clearData.addEventListener ("click", deleteData);    
+	var save = $('submit');
+	save.addEventListener("click", storeData);
     });
 
 
